76bdc89a7be12553625d438dec87b467a4ac1047
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-04-25 11:53:58 +0500

    добавлена таймзона москвы добавлены статусы

--- scr/constants.py
+++ scr/constants.py
@@ -1,6 +1,13 @@
-STATUS_COL     = "Статус публикации"
-DATE_COL       = "Дата"
-DOC_COL        = "Ссылка на документ"
-SOCIAL_COL     = "Соцсеть"
-IMAGE_COL      = "Фото URL"
+STATUS_COL      = "Статус публикации"
+DATE_COL        = "Дата"
+TIME_COL        = "Время"
+DOC_COL         = "Ссылка на документ"
+SOCIAL_COL      = "Соцсеть"
+IMAGE_COL       = "Фото URL"
 EXTRA_PAGES_COL = "Дополнительная страница"
+
+from pytz import timezone
+LOCAL_TZ = timezone("Europe/Moscow")
+
+OK_MARK    = "posted"
+FAIL_MARK  = "failed"
