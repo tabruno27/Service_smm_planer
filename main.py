2584666ecd6d7484f25406a1f0467951cf26651e
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-04-27 08:34:05 +0500

    удалил старый закомменированный код

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# from scr.scheduler import process_schedule
-# from environs import Env
-#
-# def main():
-#     env = Env()
-#     env.read_env(path='token.env')
-#     VK_TOKEN = env.str("VK_TOKEN")
-#     TELEGRAM_TOKEN = env.str("TELEGRAM_TOKEN")
-#     VK_GROUP_ID = env.int("VK_GROUP_ID")
-#     TELEGRAM_CHANNEL_ID = env.str("TELEGRAM_CHANNEL_ID")
-#     process_schedule(VK_TOKEN, TELEGRAM_TOKEN, VK_GROUP_ID, TELEGRAM_CHANNEL_ID)
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
 from environs import Env
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pytz import timezone
